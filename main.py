eb3db75e33f1b9e12257c7d34793a366638539cf
Oleg Selivanov <alien3569@mail.ru>
2023-03-08 19:00:37 +0300

    Add Seminar7

--- main.py
+++ main.py
@@ -798,3 +798,140 @@ Output: 4 3
 #         print(i, m)
 
 
+
+
+# # Семинар 7
+
+
+"""
+Задача №47. Решение в группах
+У вас есть код, который вы не можете менять(так часто бывает, когда код в глубине
+программы используется множество раз и вы не хотите ничего сломать):
+transformation = <???>
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+или любой другой список transormed_values = list(map(transformation, values))
+Единственный способ вашего взаимодействия с этим кодом - посредством задания
+функции transformation. Однако вы поняли, что для вашей текущей задачи вам не нужно
+никак преобразовывать список значений, а нужно получить его как есть.
+Напишите такое лямбда-выражение transformation, чтобы transformed_values получился
+копией values.
+
+Ввод:
+values = [1, 23, 42, ‘asdfg’]
+transformed_values = list(map(trasformation, values))
+if values == transformed_values:
+    print(‘ok’)
+else:
+    print(‘fail’)
+
+Вывод:
+ok
+"""
+
+# transformation = lambda x: x                # вводим в код строку, lambda (анонимную) функцию которая ничего не делает х=х
+# values = [1, 23, 42, 'asdfg']                           # чтобы не писать return
+# transformed_values = list(map(transformation, values))  #list функция, которая оборачивает в список работу map
+#                                                         # если не поставить list, то выдаст объект map <map object at 0x000000B67783B070>                                                    
+#                                                         # map применяет функцию transformation, на 
+#                                                         # каждый элемент списка values
+# if values == transformed_values:
+#     print('ok')
+# else:
+#     print('fail')
+
+# print(values)
+# print(transformed_values)
+
+
+"""
+Задача №49. Решение в группах
+Планеты вращаются вокруг звезд по эллиптическим орбитам. Назовем самой далекой планетой ту,
+орбита которой имеет самую большую площадь. Напишите функцию find_farthest_orbit(list_of_orbits),
+которая среди списка орбит планет найдет ту, по которой вращается самая далекая планета.
+Круговые орбиты не учитывайте: вы знаете, что у вашей звезды таких планет нет, зато искусственные 
+спутники были были запущены на круговые орбиты.
+Результатом функции должен быть кортеж, содержащий длины полуосей эллипса орбиты самой далекой
+планеты. Каждая орбита представляет из себя кортеж из пары чисел - полуосей ее эллипса.
+Площадь эллипса вычисляется по формуле S = pi*a*b, где a и b - длины полуосей эллипса.
+При решении задачи используйте списочные выражения.
+Подсказка: проще всего будет найти эллипс в два шага: сначала вычислить самую большую 
+площадь эллипса, а затем найти и сам эллипс, имеющий такую площадь.
+Гарантируется, что самая далекая планета ровно одна.
+
+Ввод:
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+print(*find_farthest_orbit(orbits))
+
+Вывод:
+2.5  10
+"""
+
+# # 1 вариант
+
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+
+# def find_farthest_orbit(orbits):
+#    pi = 3.14
+#    orbits_new = [(0, 0)]
+#    max = 0
+#    for i in orbits:
+#         if i[0] != i[1]:
+#             if max < pi * i[0] * i[1]:
+#                max = pi * i[0] * i[1]
+#                orbits_new.pop()             # если выполняется условие, то удаляем предыдущее значение кортежа
+#                orbits_new.append(i)         # и добавляем новое, чтобы в списке оставалась только одно, max
+#    return orbits_new[0]                     # добавили [0], чтобы распаковывался список
+
+# print(*find_farthest_orbit(orbits))
+
+
+# # 2 вариант (убрали число pi, т.к. это не влияет на нахождение max)
+
+# def find_farthest_orbit(list_of_orbits):
+#     list_of_elliptical_orbits = [i for i in list_of_orbits if i[0] != i[1]]     # возвращает число из списка, который
+#                                                                                 # он принимает, где 1 элемент не равен 2
+#     list_of_areas = [(i[0] * i[1]) for i in list_of_elliptical_orbits]          # возвращает произведение элементов из пред. списка
+#     max_area_index = list_of_areas.index(max(list_of_areas))                    # находим max из пред.списка
+#     return list_of_elliptical_orbits[max_area_index]                            # возвращает max элемент
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+# print(*find_farthest_orbit(orbits))
+
+
+
+"""
+Задача №51. Решение в группах
+апишите функцию same_by(characteristic, objects), которая проверяет, все ли объекты имеют
+одинаковое значение некоторой характеристики, и возвращают True, если это так. Если значение
+характеристики для разных объектов отличается - то False. Для пустого набора объектов, функция
+должна возвращать True. Аргумент characteristic - это функция, которая принимает объект
+и вычисляет его характеристику.
+
+Ввод: 
+values = [0, 2, 10, 6] 
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')
+
+Вывод:
+same
+"""
+
+# def same_by(charac, objects):                                   # charac это lambda x: x % 2, objects это values
+#     list_characteristic = [charac(i) for i in objects]          # list comprehension - генератор списка
+#     for i in range(len(list_characteristic) - 1):
+#         if list_characteristic[i] != list_characteristic[i+1]:
+#             return False
+#         return True
+    
+# values = [0, 2, 10, 6]
+
+# if same_by(lambda x: x % 2, values):
+#     print('same')
+# else:
+#     print('different')
+
+
+# # list_characteristic = [charac(i) for i in objects]    # list comprehension - генератор списка
+# # обратная запись, мы перед for ставим то, что будем заполнять через функцию и подаем параметр,
+# # так как это фукнция в нашем случает это 0 % 2, 2 % 2, 10 % 2)
